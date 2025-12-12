d66fef9798e34f0dfed5c1d0c576d8114ea31d09
Nathan Shan <nathanielcrush51@gmail.com>
2025-12-12 17:33:54 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base:'/nateEc.github.io',
+  base:'/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
